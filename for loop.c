0757f74044ae379ed096374732e78bcc99a5a2e5
RiyaRaut17 <riyaraut1707@gmail.com>
2025-07-18 12:57:49 +0530

    Create for loop.c

---
+++ for loop.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() { int i;
+
+// Loop from 1 to 9
+for(i = 1; i < 10; i++) 
+{
+    
+    // Check if i is equal to 5
+    if(i == 5) 
+    {
+        
+        switch(i)
+         {
+             break;
+         }
+    } 
+    else
+    {
+        // Print all other numbers
+        printf("Number: %d\n", i);
+    }
+}
+
+return 0;
+}
